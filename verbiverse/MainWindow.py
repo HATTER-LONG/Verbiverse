cd62d7a376702f7eab00fffda57444cb87122642
Layton <caolei6767@gmail.com>
2024-04-21 20:53:25 +0800

    [Feature] base UI

--- verbiverse/MainWindow.py
+++ verbiverse/MainWindow.py
@@ -15,114 +15,68 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
-    QMainWindow, QMenuBar, QPlainTextEdit, QPushButton,
-    QScrollArea, QSizePolicy, QSpacerItem, QStatusBar,
-    QTextBrowser, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QMenuBar,
+    QPushButton, QScrollArea, QSizePolicy, QStatusBar,
+    QTextEdit, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(705, 564)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout_3 = QVBoxLayout(self.centralwidget)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.frame = QFrame(self.centralwidget)
-        self.frame.setObjectName(u"frame")
-        self.frame.setStyleSheet(u"background-color: rgb(246, 246, 246);\n"
-"border-radius:20px;\n"
-"border:3px solid #34495e;\n"
-"")
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_2 = QVBoxLayout(self.frame)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.scrollArea = QScrollArea(self.frame)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setStyleSheet(u"border:initial;\n"
-"border: 0px solid;")
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 758, 398))
-        self.verticalLayout = QVBoxLayout(self.scrollAreaWidgetContents)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.widget = QWidget(self.scrollAreaWidgetContents)
-        self.widget.setObjectName(u"widget")
-        self.widget.setMinimumSize(QSize(600, 200))
-        self.widget.setMaximumSize(QSize(600, 200))
-        self.verticalLayout_5 = QVBoxLayout(self.widget)
+        self.verticalLayout_6 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.verticalLayout_5 = QVBoxLayout()
         self.verticalLayout_5.setObjectName(u"verticalLayout_5")
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.verticalLayout_4 = QVBoxLayout()
-        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
-        self.label = QLabel(self.widget)
-        self.label.setObjectName(u"label")
-        self.label.setMaximumSize(QSize(50, 50))
-        self.label.setPixmap(QPixmap(u"1.jpg"))
-        self.label.setScaledContents(True)
+        self.chatScrollArea = QScrollArea(self.centralwidget)
+        self.chatScrollArea.setObjectName(u"chatScrollArea")
+        self.chatScrollArea.setWidgetResizable(False)
+        self.chatScrollArea.setAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
+        self.scrollAreaWidgetContents_2 = QWidget()
+        self.scrollAreaWidgetContents_2.setObjectName(u"scrollAreaWidgetContents_2")
+        self.scrollAreaWidgetContents_2.setGeometry(QRect(0, 0, 677, 392))
+        self.verticalLayout = QVBoxLayout(self.scrollAreaWidgetContents_2)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.chatScrollArea.setWidget(self.scrollAreaWidgetContents_2)
 
-        self.verticalLayout_4.addWidget(self.label)
+        self.verticalLayout_5.addWidget(self.chatScrollArea)
 
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Preferred)
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.userTextEdit = QTextEdit(self.centralwidget)
+        self.userTextEdit.setObjectName(u"userTextEdit")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.userTextEdit.sizePolicy().hasHeightForWidth())
+        self.userTextEdit.setSizePolicy(sizePolicy)
 
-        self.verticalLayout_4.addItem(self.verticalSpacer)
+        self.horizontalLayout_5.addWidget(self.userTextEdit)
+
+        self.userSendButton = QPushButton(self.centralwidget)
+        self.userSendButton.setObjectName(u"userSendButton")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Maximum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.userSendButton.sizePolicy().hasHeightForWidth())
+        self.userSendButton.setSizePolicy(sizePolicy1)
+        self.userSendButton.setMinimumSize(QSize(0, 0))
+        self.userSendButton.setMaximumSize(QSize(16777215, 50))
+
+        self.horizontalLayout_5.addWidget(self.userSendButton)
 
 
-        self.horizontalLayout_2.addLayout(self.verticalLayout_4)
-
-        self.textBrowser = QTextBrowser(self.widget)
-        self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setStyleSheet(u"background-color: rgba(71,121,214,20);")
-
-        self.horizontalLayout_2.addWidget(self.textBrowser)
+        self.verticalLayout_5.addLayout(self.horizontalLayout_5)
 
 
-        self.verticalLayout_5.addLayout(self.horizontalLayout_2)
+        self.verticalLayout_6.addLayout(self.verticalLayout_5)
 
-
-        self.verticalLayout.addWidget(self.widget)
-
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-
-        self.verticalLayout_2.addWidget(self.scrollArea)
-
-
-        self.verticalLayout_3.addWidget(self.frame)
-
-        self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.plainTextEdit = QPlainTextEdit(self.centralwidget)
-        self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.setStyleSheet(u"QPlainTextEdit{\n"
-"	border-radius:20px;\n"
-"	border:3px solid #2c3e50;\n"
-"	background-color: transparent;\n"
-"	font: 12pt \"\u5fae\u8f6f\u96c5\u9ed1\";\n"
-"	padding:5px;\n"
-"}")
-
-        self.horizontalLayout.addWidget(self.plainTextEdit)
-
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-
-        self.horizontalLayout.addWidget(self.pushButton)
-
-        self.horizontalLayout.setStretch(0, 5)
-        self.horizontalLayout.setStretch(1, 1)
-
-        self.verticalLayout_3.addLayout(self.horizontalLayout)
-
-        self.verticalLayout_3.setStretch(0, 5)
-        self.verticalLayout_3.setStretch(1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 26))
+        self.menubar.setGeometry(QRect(0, 0, 705, 24))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -135,7 +89,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label.setText("")
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.userSendButton.setText(QCoreApplication.translate("MainWindow", u"Send", None))
     # retranslateUi
 
--- verbiverse/__init__.py
+++ verbiverse/__init__.py
@@ -1,7 +1,8 @@
 import sys
 
 from MainWindow import Ui_MainWindow
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QVBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -9,9 +10,49 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        self.setWindowTitle("聊天界面")
+
+        self.messages_list_widget = QWidget()
+        self.messages_list = QVBoxLayout(
+            self.messages_list_widget
+        )  # Change to QVBoxLayout
+        self.messages_list.setAlignment(Qt.AlignTop)
+
+        self.messages_list_widget.setContentsMargins(0, 0, 0, 0)
+
+        self.chatScrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.vscrollbar = self.chatScrollArea.verticalScrollBar()
+        self.vscrollbar.rangeChanged.connect(self.scrollToBottomIfNeeded)
+        self.adding = False
+
+        self.chatScrollArea.setWidgetResizable(True)
+        self.chatScrollArea.setWidget(self.messages_list_widget)
+
+        self.userTextEdit.setPlaceholderText("输入消息")
+        self.userSendButton.clicked.connect(self.send_Message)
+
+    def scrollToBottomIfNeeded(self, minimum, maximum):
+        if self.adding:
+            self.vscrollbar.setValue(maximum)
+            self.adding = False
+
+    def send_Message(self):
+        message_text = self.userTextEdit.toPlainText()
+        if message_text:
+            message_label = QLabel(f"You: {message_text}")
+            message_label.setWordWrap(True)
+            self.messages_list.addWidget(message_label)  # Add to QVBoxLayout
+            self.userTextEdit.setText("")
+
+            # 模拟机器人回复
+            reply_text = "你好，我收到你的消息了。"
+            reply_label = QLabel(f"Robot: {reply_text}")
+            reply_label.setWordWrap(True)
+            self.messages_list.addWidget(reply_label)  # Add to QVBoxLayout
+            self.adding = True
+
 
 app = QApplication(sys.argv)
-
 window = MainWindow()
 window.show()
 
