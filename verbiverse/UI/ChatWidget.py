c5c9e9df6b8140d615a326562c949ed1cc5c7254
Layton <caolei6767@gmail.com>
2024-06-08 11:15:31 +0800

    [Feature] add check 1/2

--- verbiverse/UI/ChatWidget.py
+++ verbiverse/UI/ChatWidget.py
@@ -1,10 +1,16 @@
 from ChatWidget_ui import Ui_ChatWidget
+from CustomWidgets.ExplainFlyoutView import ExplainFlyoutView
 from LLM.ChatChain import ChatChain
+from LLM.ChatWithCustomHistoryChain import ChatLLMWithCustomHistory
 from LLM.ChatWorkerThread import ChatWorkThread
 from MessageBox import CMessageBox
 from ModuleLogger import logger
 from PySide6.QtCore import Qt, Slot
 from PySide6.QtWidgets import QWidget
+from qfluentwidgets import (
+    Flyout,
+    FlyoutAnimationType,
+)
 
 
 class ChatWidget(QWidget, Ui_ChatWidget):
@@ -25,11 +31,19 @@ class ChatWidget(QWidget, Ui_ChatWidget):
         self.chat_worker.setChain(self.chat_chain)
         self.chat_worker.messageCallBackSignal.connect(self.updateLabel)
 
+        self.check_chain = ChatLLMWithCustomHistory()
+        self.check_worker = ChatWorkThread()
+        self.check_worker.finished.connect(self.checkFinish)
+        self.check_worker.started.connect(self.checkStart)
+        self.check_worker.setChain(self.check_chain)
+        # self.check_worker.messageCallBackSignal.connect(self.updateFlyout)
+
         self.need_update_label = None
         self.connectSignal()
 
     def connectSignal(self):
         self.user_send_button.clicked.connect(self.sendMessage)
+        self.user_check_button.clicked.connect(self.checkInput)
 
     @Slot()
     def sendMessage(self):
@@ -69,3 +83,29 @@ class ChatWidget(QWidget, Ui_ChatWidget):
             self.need_update_label.setMessageText(
                 self.need_update_label.getMessageText() + message
             )
+
+    @Slot()
+    def checkInput(self):
+        message_text = self.user_text_edit.toPlainText()
+        if message_text:
+            self.check_chain.setChatHistoryForChain(
+                self.chat_chain.demo_ephemeral_chat_history_for_chain
+            )
+            logger.info(self.chat_chain.demo_ephemeral_chat_history_for_chain)
+
+            pos = self.user_check_button.mapToGlobal(self.user_check_button.pos())
+            pos.setX(pos.x() - 200)
+            self.flyout = Flyout.make(
+                ExplainFlyoutView("Check"),
+                self.user_check_button,
+                self,
+                aniType=FlyoutAnimationType.DROP_DOWN,
+            )
+
+    @Slot()
+    def checkStart(self) -> None:
+        pass
+
+    @Slot()
+    def checkFinish(self) -> None:
+        self.need_update_label = None
