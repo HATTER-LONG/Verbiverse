a3ca2579e1a7faa3b8031526e9e95bb9237a5a32
Layton <caolei6767@gmail.com>
2024-06-09 15:45:39 +0800

    [Fix] optz build script

--- build.py
+++ build.py
@@ -8,6 +8,24 @@ from verbiverse.Functions.Log import get_logger
 
 logger = get_logger("project")
 
+# Main cfg
+PROJECT_ROOT_PATH = "./verbiverse"
+
+# Translate file cfg
+NEED_TRANSLATE_FILE_PATH_LIST = [
+    "./verbiverse/UI/SettingInterface.py",
+    "./verbiverse/main.py",
+]
+TRANSLATE_RESOURCE_PATH = "./verbiverse/resources/i18n/"
+TRANSLATE_RESULT_FILE = "./verbiverse/resources/i18n/verbiverse.zh_CN"
+
+# optimize prompt cfg
+PROMPT_RESOURCE_PATH = "./verbiverse/resources/prompt/"
+PROMPT_TASK_LIST = {
+    "chat_prompt.txt": "help me to improve a new language skill by chat",
+    "explain_prompt.txt": "explain the new language input data mean and output structed data",
+}
+
 
 def convert_ui_files(ui_file_path, py_file_path):
     """Converts a UI file (.ui) to a Python file (.py) using pyside6-uic."""
@@ -39,34 +57,38 @@ def convert_qm_files(ts_file_path, qm_file_path):
         print(f"Error converting {qm_file_path}: {e.output}")
 
 
+def update_py_to_ts(py_file_path: list[str], prefix: str, resource_path: str):
+    for py_file in py_file_path:
+        if py_file.endswith(".py"):
+            ts_file_path = py_file.replace(".py", f".TMP.{prefix}.ts")
+            ts_file_path = resource_path + os.path.basename(ts_file_path)
+            command = ["pyside6-lupdate", py_file, "-ts", ts_file_path]
+            try:
+                subprocess.run(command, check=True)
+                print(f"Successfully converted {py_file} to {ts_file_path}")
+            except subprocess.CalledProcessError as e:
+                print(f"Error converting {py_file}: {e.output}")
+
+
+def py_translate_ts():
+    update_py_to_ts(NEED_TRANSLATE_FILE_PATH_LIST, "zh_CN", TRANSLATE_RESOURCE_PATH)
+
+
 def build():
+    py_translate_ts()
+    convert_all_ts_to_one()
     qrc_file_path = []
-    for root, _, files in os.walk("."):
+    for root, _, files in os.walk(PROJECT_ROOT_PATH):
         for filename in files:
             if filename.endswith(".ui"):
                 ui_file_path = os.path.join(root, filename)
                 py_file_path = ui_file_path.replace(".ui", "_ui.py")
                 convert_ui_files(ui_file_path, py_file_path)
-            # if filename.endswith(".ts") and ".TMP." not in filename:
-            #     ts_file_path = os.path.join(root, filename)
-            #     qm_file_path = ts_file_path.replace(".ts", ".qm")
-            #     convert_qm_files(ts_file_path, qm_file_path)
             if filename.endswith(".qrc"):
                 qrc_file_path.append(os.path.join(root, filename))
-    convert_all_ts_to_one()
     for qrc_file_path in qrc_file_path:
         py_file_path = qrc_file_path.replace(".qrc", "_rc.py")
         convert_qrc_files(qrc_file_path, py_file_path)
-
-
-def run_app():
-    """Runs the main application."""
-    try:
-        import main
-
-        main.main()
-    except Exception as e:
-        print(f"Error running application: {e}")
 
 
 def demo(module_name):
@@ -79,11 +101,6 @@ def demo(module_name):
         function()
     except Exception as e:
         print(f"Error running demo: {e}")
-
-
-def test():
-    """use pytest to run unit tests"""
-    subprocess.run(["pytest"])
 
 
 def get_directory_names(directory):
@@ -101,26 +118,22 @@ def check_directory_existence(directory, name):
     return name in directory_names
 
 
-def update_py_to_ts(py_file_path: list[str], prefix: str, resource_path: str):
-    for py_file in py_file_path:
-        if py_file.endswith(".py"):
-            ts_file_path = py_file.replace(".py", f".TMP.{prefix}.ts")
-            ts_file_path = resource_path + os.path.basename(ts_file_path)
-            command = ["pyside6-lupdate", py_file, "-ts", ts_file_path]
-            try:
-                subprocess.run(command, check=True)
-                print(f"Successfully converted {py_file} to {ts_file_path}")
-            except subprocess.CalledProcessError as e:
-                print(f"Error converting {py_file}: {e.output}")
-
-
-def get_tmp_files(path):
-    tmp_files = []
-    for root, _, files in os.walk(path):
+def optimize_prompts():
+    prompts = []
+    for root, _, files in os.walk(PROMPT_RESOURCE_PATH):
         for file in files:
-            if ".TMP." in file:
-                tmp_files.append(os.path.join(root, file))
-    return tmp_files
+            if file in PROMPT_TASK_LIST:
+                prompts.append(file)
+
+    print("Select prompt you want to optimize:")
+    for index, file in enumerate(prompts):
+        print(f"{index} -> {file}")
+    index = int(input("Please input index: "))
+
+    logger.info(f"optimize prompt: {prompts[index]}")
+    optimizationPrompt(
+        PROMPT_RESOURCE_PATH + prompts[index], PROMPT_TASK_LIST[prompts[index]]
+    )
 
 
 def extract_context_tags(input_file):
@@ -132,65 +145,90 @@ def extract_context_tags(input_file):
 
 
 def convert_all_ts_to_one():
-    files = get_tmp_files("./verbiverse/resources")
+    """
+    Merges multiple temporary translation files into a single translation file.
+    This function searches for temporary translation files (`*.TMP.ts`) in the `TRANSLATE_RESOURCE_PATH` directory and its subdirectories. It reads the content of the first temporary file, extracts the context tags using the `extract_context_tags()` function, and stores them in the `firstcontext` variable.
+    Then, it iterates over the remaining temporary files and appends their context tags to the `content` variable.
+    Finally, it replaces the context tags in the `firstfile` with the `content` and writes the result to the `TRANSLATE_RESULT_FILE.ts` file.
+    After that, it calls the `convert_qm_files()` function to convert the merged translation file to a QM file (`TRANSLATE_RESULT_FILE.qm`).
+    """
+    ts_files = [
+        os.path.join(root, file)
+        for root, _, files in os.walk(TRANSLATE_RESOURCE_PATH)
+        for file in files
+        if ".TMP." in file
+    ]
+
     firstfile = ""
     content = ""
     firstcontext = ""
 
-    with open(files[0], "r", encoding="utf-8") as f:
+    with open(ts_files[0], "r", encoding="utf-8") as f:
         firstfile = f.read()
-        firstcontext = extract_context_tags(files[0])
+        firstcontext = extract_context_tags(ts_files[0])
 
-    for file in files:
-        content = content + extract_context_tags(file)
-
+    content = "".join(extract_context_tags(file) for file in ts_files)
     result = firstfile.replace(firstcontext, content)
 
-    with open(
-        "./verbiverse/resources/i18n/verbiverse.zh_CN.ts", "w", encoding="utf-8"
-    ) as f:
+    with open(f"{TRANSLATE_RESULT_FILE}.ts", "w", encoding="utf-8") as f:
         f.seek(0)
         f.truncate()
         f.write(result)
 
     convert_qm_files(
-        "./verbiverse/resources/i18n/verbiverse.zh_CN.ts",
-        "./verbiverse/resources/i18n/verbiverse.zh_CN.qm",
+        f"{TRANSLATE_RESULT_FILE}.ts",
+        f"{TRANSLATE_RESULT_FILE}.qm",
     )
 
 
-def optimize_prompt(promptPath, task):
+def optimizationPrompt(prompt_path, task):
+    """
+    Optimizes a prompt file by generating an optimized prompt and asking for user confirmation to overwrite the original prompt.
+
+    Args:
+        prompt_path (str): The path to the prompt file.
+        task (str): The task associated with the prompt.
+
+    Returns:
+        None
+    """
     import datetime
 
     from tools.prompt_maker import promptMaker
 
-    logger.info(f"prompt path: {promptPath}, task: {task}")
-
-    res = ""
-    with open(promptPath, "r+", encoding="utf-8") as f:
+    # Read the content of the prompt file
+    with open(prompt_path, "r", encoding="utf-8") as f:
         content = f.read()
-        res = promptMaker(content, task).strip()
-        logger.info(f"res: \n\n{res}\n\n")
-        if input("overwrite the prompt? y/n:").lower() == "y":
-            time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-            with open(f"{promptPath}_{time}.bak", "w", encoding="utf-8") as bak:
-                bak.write(content)
-                logger.info(f"already backup to {promptPath}_{time}.bak")
-            f.truncate(0)
-            f.seek(0)
-            f.write(res)
-        f.flush()
-    logger.info("prompt optimization done")
+
+    logger.info(
+        f"start optimize prompt, please wait. -> path: {prompt_path}, task: {task}"
+    )
+
+    # Generate the optimized prompt
+    optimized_prompt = promptMaker(content, task).strip()
+    # Ask user for confirmation to overwrite the prompt
+    if input("Overwrite the prompt? (y/n): ").lower() == "y":
+        # Create a backup of the original prompt
+        time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        with open(f"{prompt_path}_{time}.bak", "w", encoding="utf-8") as bak:
+            bak.write(content)
+            logger.info(f"Backup created at {prompt_path}_{time}.bak")
+
+        # Write the optimized prompt back to the original file
+        with open(prompt_path, "w", encoding="utf-8") as f:
+            f.write(optimized_prompt)
+
+        logger.info("Prompt optimization completed and saved.")
+    else:
+        logger.info("Prompt optimization canceled.")
 
 
 def main():
     """Parses command-line arguments and executes corresponding actions."""
-    parser = argparse.ArgumentParser(
-        description="Engineering Project Management Script"
-    )
+    parser = argparse.ArgumentParser(description="Project Management Script")
     parser.add_argument(
         "command",
-        choices=["run", "build", "br", "test", "demo", "pyts", "prompt"],
+        choices=["build", "demo", "pyts", "prompt"],
         help="Command to execute",
     )
     parser.add_argument("name", nargs="?", default=None, help="Name for demo")
@@ -199,13 +237,6 @@ def main():
 
     if args.command == "build":
         build()
-    elif args.command == "run":
-        run_app()
-    elif args.command == "br":
-        build()
-        run_app()
-    elif args.command == "test":
-        test()
     elif args.command == "demo":
         tests_directory = "./tests/UI/"
         if check_directory_existence(tests_directory, args.name):
@@ -214,36 +245,11 @@ def main():
         else:
             logger.error("Demo command requires a name of tests/UI/[name]")
     elif args.command == "pyts":
-        # cn_ts_file_path = "./resources/i18n/verbiverse.zh_CN.ts"
-        # hk_ts_file_path = "./resources/i18n/verbiverse.zh_HK.ts"
-        py_file_path = ["./verbiverse/UI/SettingInterface.py", "./verbiverse/main.py"]
-        resource_path = "./verbiverse/resources/i18n/"
-        update_py_to_ts(py_file_path, "zh_CN", resource_path)
-        # update_py_to_ts(py_file_path, "zh_HK", resource_path)
+        py_translate_ts()
     elif args.command == "prompt":
-        if not args.name:
-            logger.error("Please specify the name of the prompt.")
-        else:
-            logger.info(f"optimize prompt: {args.name}")
-
-            promptPrefix = "./verbiverse/resources/prompt/"
-            promptList = [
-                {
-                    "name": "chat_prompt",
-                    "task": "help me to improve a new language skill by chat",
-                },
-                {
-                    "name": "explain_prompt",
-                    "task": "explain the new language input data mean and output structed data",
-                },
-            ]
-            for prompt in promptList:
-                if prompt["name"] == args.name:
-                    optimize_prompt(
-                        promptPrefix + prompt["name"] + ".txt", prompt["task"]
-                    )
+        optimize_prompts()
     else:
-        logger.error("Invalid command. Use 'run', 'build', 'test', 'br', or 'demo'.")
+        logger.error("Invalid command.")
 
 
 if __name__ == "__main__":
